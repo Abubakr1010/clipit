b25d8359ee12b33d9758fa5a1d14c97f838fbb1d
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-10-31 13:50:45 +0100

    Created Video Model

--- apis/models.py
+++ apis/models.py
@@ -30,10 +30,27 @@ class User(AbstractBaseUser, PermissionsMixin ):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    email = models.EmailFieldField(max_length=100)
     profile_image = models.URLField(max_length=500, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+class Video(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    link = models.URLField(max_length=500)
+
+
+
+
 
 
     
