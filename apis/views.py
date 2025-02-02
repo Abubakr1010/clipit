b87212f8fb3591edd3f5e805210769eae5b6e386
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-02-02 22:31:49 +0100

    filter 12

--- apis/views.py
+++ apis/views.py
@@ -327,6 +327,14 @@ class FiltersViewSet(viewsets.viewset):
                             return Response({"status":"updated successfully"}, status=status.HTTP_200_OK)
 
 
+               except Exception as e:
+            return Response({'error':str(e)},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+                            return Response({"status":"updated successfully"}, status=status.HTTP_200_OK)
+
+
         
 
 
