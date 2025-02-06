6f90f1623d85b2e192d9da807b8efad4edda9451
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-02-06 22:14:48 +0100

    filter 13

--- apis/views.py
+++ apis/views.py
@@ -346,6 +346,10 @@ class FiltersViewSet(viewsets.viewset):
                except Exception as e:
             return Response({'error':str(e)},
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        except Exception as e:
+            return Response({'error':str(e)},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
